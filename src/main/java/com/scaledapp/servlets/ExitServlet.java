632a89aa82be4ad1ad382fd47731f3cc66df07d5
Shekhar Gulati <shekhargulati84@gmail.com>
2014-08-26 00:13:48 +0530

    added ExitServlet

---
+++ src/main/java/com/scaledapp/servlets/ExitServlet.java
@@ -0,0 +1,34 @@
+package com.scaledapp.servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ExitServlet
+ */
+@WebServlet("/ExitServlet")
+public class ExitServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ExitServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    System.exit(-1);
+	}
+
+	
+
+}
